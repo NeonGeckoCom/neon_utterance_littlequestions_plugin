758d62b1d80005d0800bee593d7e922795862b7e
jarbasai <jarbasai@mailfence.com>
2021-11-19 12:48:30 +0000

    :tada:

---
+++ neon_utterance_littlequestions_plugin/__init__.py
@@ -0,0 +1,45 @@
+# # NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+# # All trademark and other rights reserved by their respective owners
+# # Copyright 2008-2021 Neongecko.com Inc.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from neon_transformers import UtteranceTransformer
+from little_questions.classifiers import get_classifier
+
+
+class QuestionTagger(UtteranceTransformer):
+    def __init__(self, name="little_questions", priority=50):
+        super().__init__(name, priority)
+        self.classifiers = {"en": get_classifier("en")}
+
+    def load_classifier(self, lang):
+        lang = lang.split("-")[0]
+        if lang not in self.classifiers:
+            self.classifiers[lang] = get_classifier(lang)
+        return self.classifiers[lang]
+
+    def transform(self, utterances, context=None):
+        context = context or {}
+        lang = context.get("lang") or self.config.get("lang", "en-us")
+        classifier = self.load_classifier(lang)
+        preds = [classifier.predict([utt])[0] for utt in utterances]
+        return utterances, {"question_type": preds}
---
+++ readme.md
@@ -0,0 +1,6 @@
+```python
+
+question = "who made you"
+print(QuestionTagger().transform([question]))
+# (['who made you'], {'question_type': ['HUM:ind']})
+```
